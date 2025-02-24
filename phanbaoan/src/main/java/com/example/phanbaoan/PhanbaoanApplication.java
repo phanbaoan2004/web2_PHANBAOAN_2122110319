ce0a46241fa1f977e9977d2d1fce55274c2519ea
phanbaoan <phanbaoa@gmail.com>
2025-02-24 10:27:22 +0700

    first commit

---
+++ phanbaoan/src/main/java/com/example/phanbaoan/PhanbaoanApplication.java
@@ -0,0 +1,13 @@
+package com.example.phanbaoan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhanbaoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhanbaoanApplication.class, args);
+	}
+
+}
---
+++ phanbaoan/src/test/java/com/example/phanbaoan/PhanbaoanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.phanbaoan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhanbaoanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
